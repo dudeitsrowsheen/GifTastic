02638ca56b959d397a8888506c7c18ccab940b86
dudeitsrowsheen <48970034+dudeitsrowsheen@users.noreply.github.com>
2019-05-19 22:00:49 -0500

    GifTastic

---
+++ App/assets/js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {//I need jquery to run!
+
+	var searchTerms = ['fox', 'cow', 'chicken', 'pig'];
+	console.log("testing...");
+
+	var createButton = function (name) {
+		var button = $('<button>');
+		button.text(name);
+		button.click(function () {
+			gifQuery($(this).text());
+
+		})
+		$('.button-container').append(button);
+	}
+
+	var createButtonsFromArr = function (arr) {
+		for (var i = 0; i < arr.length; i++) {
+			createButton(arr[i]);
+		}
+
+	}
+
+	var gifQuery = function (term) {
+		var apiKey = "lGrsxp8P15M96FdeFE7w9zMmmmbvJHaA";
+		var queryURL = "http://api.giphy.com/v1/gifs/search?apiKey="
+			+ apiKey
+			+ "&q=" + term;
+		console.log(queryURL);
+
+		$.ajax({
+			method: "GET",
+			url: queryURL,
+
+		}).then(function (result) {
+		var gif = $('<img>')
+		gif.attr("src",result.data[0].images.fixed_width.url);
+		// for loop for more images
+		$(".gifs").append(gif);
+	
+
+
+		});
+	}
+	gifQuery("dog");
+
+	createButtonsFromArr(searchTerms);
+
+	$("#search-button").on('click', function(){
+		// targeting
+		var searchTerm = $("#search-field").val();
+			console.log(searchTerm);
+			gifQuery(searchTerm);
+
+
+	})
+})
